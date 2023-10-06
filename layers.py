5e0f75a24bd060e1667d1053fdd13a2905a30859
kazuki-naka <kazunaka.0077@gmail.com>
2023-10-06 15:35:08 +0900

    LoRALayerの中身を整理しただけ

--- layers.py
+++ layers.py
@@ -4,17 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LoRALayer():
-    def __init__(
-        self, 
-        r: int, 
-        lora_alpha: int, 
-        lora_dropout: float,
-        merge_weights: bool,
-    ):
+    def __init__(self, r: int, lora_alpha: int, lora_dropout: float,merge_weights: bool,):
         self.r = r
         self.lora_alpha = lora_alpha
         # Optional dropout
-        if lora_dropout > 0.:
+        if lora_dropout > 0:
             self.lora_dropout = nn.Dropout(p=lora_dropout)
         else:
             self.lora_dropout = lambda x: x
